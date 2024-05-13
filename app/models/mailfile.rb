49d69bb4cad5246a0935480987ea573c64515ed0
abertelsens <50672240+abertelsens@users.noreply.github.com>
2024-05-13 16:26:09 +0200

    pdf viewer

--- app/models/mailfile.rb
+++ app/models/mailfile.rb
@@ -39,7 +39,7 @@ class MailFile < ActiveRecord::Base
 
 
 	def	get_pdf_path
-		puts "asking pdf file"
+		puts "asking pdf file. copying #{mail.get_sources_directory}/#{name} to  app/public/tmp/mail"
 		if is_pdf_file?
 			original_file = "#{mail.get_sources_directory}/#{name}"
 			FileUtils.cp(original_file, "app/public/tmp/mail")
