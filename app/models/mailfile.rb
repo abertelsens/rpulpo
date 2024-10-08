99114fb010b4a2d3db170331ce5c5da011eb566b
abertelsens <50672240+abertelsens@users.noreply.github.com>
2024-10-08 11:25:42 +0200

    several changes in mail classes

--- app/models/mailfile.rb
+++ app/models/mailfile.rb
@@ -41,7 +41,7 @@ class MailFile < ActiveRecord::Base
 		if is_word_file?
 			html_string = `pandoc --email-obfuscation=none --extract-media tmp \"#{get_path}\" --from docx --to html`
 			puts clean_html html_string
-			clean_html_links html_string
+			clean_html html_string
 		else
 			"<p> could not parse document"
 		end
