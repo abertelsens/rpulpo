d4da8caf7921c7766a36efee4fe17532387455b4
abertelsens <50672240+abertelsens@users.noreply.github.com>
2024-12-08 18:04:48 +0100

    polishing

--- run_app.sh
+++ run_app.sh
@@ -1,2 +1,7 @@
 # shell script to run the application.
-puma -p 2948 -e production
+# nohup will run the command but keeping it alive if the shell session ends.
+# the > /dev/null 2>&1& is sending the output to a nill device. What is does
+# is basically to ignore the output. Otherwise the output file will be too heavy
+nohup puma -p 2948 -e production > /dev/null 2>&1&
+
+#puma -p 2948 -e production
